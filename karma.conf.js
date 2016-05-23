5b2227920e9fd6d63960417c6067bfc7395241be
Will Mendes <willmendesneto@gmail.com>
2016-05-24 01:02:30 +1000

    Loading files using package.json configuration

--- karma.conf.js
+++ karma.conf.js
@@ -8,19 +8,15 @@ module.exports = function(config) {
 
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
-    
+
     preprocessors: {
       'app/scripts/**/*.js': ['coverage']
     },
 
     // list of files / patterns to load in the browser
     files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/spec/**/*.js'
